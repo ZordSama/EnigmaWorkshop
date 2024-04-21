9e69792676b1652c4abde1b699b985fc66d04162
Zord <quangsang.red@gmail.com>
2024-04-21 12:46:34 +0700

    Revert "middle ware update"
    This reverts commit 5011103162edc957756dbab7ecefe6f334038f11.

--- EnigmaWorkshop.Frontend/app/page.tsx
+++ EnigmaWorkshop.Frontend/app/page.tsx
@@ -5,8 +5,11 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import axios from "axios";
 
 export default function Home() {
+	axios.get('http://localhost:5070/WeatherForecast').then(response => {
+	});
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
--- EnigmaWorkshop.Frontend/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-
-export function middleware(request: NextRequest){
-    if (request.nextUrl.pathname.match('/')){
-        return NextResponse.redirect(new URL('/home', request.url));
-    }
-}
